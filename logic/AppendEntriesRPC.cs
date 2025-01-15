b76269dcf6b24a743dfcc27310ca98c61baaf9f0
MJebran <jebran.mustafa@gmail.com>
2025-01-15 08:42:01 -0700

    Testing # 3

--- logic/AppendEntriesRPC.cs
+++ logic/AppendEntriesRPC.cs
@@ -4,13 +4,14 @@ public class AppendEntriesRPC
     public int Term { get; private set; }
     public List<LogEntry> Entries { get; private set; }
 
-  public AppendEntriesRPC(Guid leaderId, int term, List<LogEntry> entries)
-  {
-      LeaderId = leaderId;
-      Term = term;
-      Entries = entries ?? new List<LogEntry>();
-  }
+    public AppendEntriesRPC(Guid leaderId, int term, List<LogEntry> entries)
+    {
+        LeaderId = leaderId;
+        Term = term;
+        Entries = entries ?? new List<LogEntry>();
+    }
 }
+
 public class AppendEntriesResponse
 {
     public bool Success { get; set; }
--- logic/RaftNode.cs
+++ logic/RaftNode.cs
@@ -24,7 +24,7 @@ public class RaftNode
         CurrentTerm = 0;
         CurrentLeaderId = null;
         VotedFor = null;
-        ResetElectionTimer();
+        // ResetElectionTimer();
     }
 
     // Test #1: Node Initialization
@@ -55,78 +55,5 @@ public class RaftNode
 }
 
 
-    // Test #4: AppendEntries Response
-    public AppendEntriesResponse ProcessAppendEntries(AppendEntriesRPC rpc)
-    {
-        HandleAppendEntries(rpc);
-        return new AppendEntriesResponse { Success = true };
-    }
-
-    // Test #5: Voting for Previous Term
-    public RequestForVoteResponse HandleRequestForVote(RequestForVoteRPC rpc)
-    {
-        if (rpc.Term < CurrentTerm)
-        {
-            return new RequestForVoteResponse { VoteGranted = false };
-        }
-
-        if (rpc.Term > CurrentTerm)
-        {
-            CurrentTerm = rpc.Term;
-            VotedFor = rpc.CandidateId;
-            return new RequestForVoteResponse { VoteGranted = true };
-        }
-
-        if (VotedFor == null || VotedFor == rpc.CandidateId)
-        {
-            VotedFor = rpc.CandidateId;
-            return new RequestForVoteResponse { VoteGranted = true };
-        }
-
-        return new RequestForVoteResponse { VoteGranted = false };
-    }
-
-    // Test #6: Randomized Election Timeout
-    public void ResetElectionTimer()
-    {
-        var random = new Random();
-        ElectionTimeout = random.Next(150, 301); 
-    }
-
-    // Test #7: Term Increment on Election Start
-    public void StartElection()
-    {
-        BecomeCandidate();
-    }
-
-    // Test #8: Follower Receiving Later Term AppendEntries
-    public void ProcessAppendEntriesWithLaterTerm(AppendEntriesRPC rpc)
-    {
-        if (rpc.Term > CurrentTerm)
-        {
-            CurrentTerm = rpc.Term;
-            State = NodeState.Follower;
-            CurrentLeaderId = rpc.LeaderId;
-        }
-    }
-
-    // Test #9: Duplicate Vote Request Handling
-    public bool IsDuplicateVote(RequestForVoteRPC rpc)
-    {
-        return rpc.Term == CurrentTerm && VotedFor != null && VotedFor != rpc.CandidateId;
-    }
-
-    // Test #10: Election Timer Expiry
-    public void StartElectionTimer(int timeoutMs)
-    {
-        LastHeartbeat = DateTime.UtcNow.AddMilliseconds(-timeoutMs);
-    }
-
-    public void CheckElectionTimeout()
-    {
-        if (State == NodeState.Follower && (DateTime.UtcNow - LastHeartbeat).TotalMilliseconds > ElectionTimeout)
-        {
-            StartElection();
-        }
-    }
+    
 }
